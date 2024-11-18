91506f9279d15e9cf71aacbad3c126a8fd974a7d
izzahbalqis112 <n.izzahbalqis@gmail.com>
2024-11-18 15:15:34 +0800

    Update eventAll.dart

--- lib/pages/event/event/widget/eventAll.dart
+++ lib/pages/event/event/widget/eventAll.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:tf_pdpppms/common/appColors.dart';
-
 import '../eventPage.dart';
 import 'eventModel.dart';
 
